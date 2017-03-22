aa019e5bac8529949889aac9b4ca2db1095928e5
duandun <duandun@gmail.com>
2017-03-23 01:43:39 +0800

    update

--- gitpush.sh
+++ gitpush.sh
@@ -18,8 +18,22 @@ fi
 
 read -p "push to origin " branch
 if [[ $branch ]]; then
-    echo "pushing to origin $branch ..."
-    git push -u origin $branch
+    CURRENT_BRANCH=$(git branch | grep '^\*\s\w\+')
+    if [[ $CURRENT_BRANCH != $branch ]]; then
+        read -p "current branch is $CURRENT_BRANCH , do you want to push to $branch ? (y/n)" CONTINUE
+        if [[ $CONTINUE == "y" ]]; then
+        #    git push origin $branch
+            echo "push origin $branch"
+            exit 0
+        else
+            exit 0
+        fi
+    else
+        echo "push to $CURRENT_BRANCH"
+        exit 0
+    fi
+    #echo "pushing to origin $branch ..."
+    #git push -u origin $branch
 else
     echo "canceled push"
     exit 0
